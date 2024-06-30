6cfe188e37fd3e1347578bd7d39674f74bff5d2a
Yi-Hsien Yu <yi-hsienyu@Yi-HsiendeMacBook-Air.local>
2024-06-30 23:44:08 +0800

    fix; typing error

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -3,11 +3,11 @@
     <div class="wavy">
       <span style="--i:1;">L</span>
       <span style="--i:2;">o</span>
-      <span style="--i:3;">d</span>
-      <span style="--i:4;">i</span>
-      <span style="--i:5;">n</span>
-      <span style="--i:6;">g</span>
-      <span style="--i:7;">.</span>
+      <span style="--i:3;">a</span>
+      <span style="--i:4;">d</span>
+      <span style="--i:5;">i</span>
+      <span style="--i:6;">n</span>
+      <span style="--i:7;">g</span>
       <span style="--i:8;">.</span>
       <span style="--i:9;">.</span>
       <span style="--i:10;">.</span>
